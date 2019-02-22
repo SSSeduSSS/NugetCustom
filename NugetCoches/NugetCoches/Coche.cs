d3b7e0d5883f37bd0d7fd874bd56ad0e1589c2d7
Eduardo Bermúdez Blanco <Eduardo.Bermudez@tajamar365.com>
2019-02-22 09:44:59 +0100

    a

---
+++ NugetCoches/NugetCoches/Coche.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NugetCoches
+{
+    public class Coche
+    {
+
+        public int IdCoche { get; set; }
+        public String Marca { get; set; }
+        public String Modelo { get; set; }
+        public String Imagen { get; set; }
+
+
+    }
+}
